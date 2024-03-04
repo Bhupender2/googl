217f30c974c1cbdc71b34adc9a525a322cb4cadf
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-04 17:39:41 +0530

    googl

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"; // it contains API used to render the react component on the client side
+import App from "./App";
+import "./global.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Results from "./components/Results";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); //react 18 or above we do it like this-- creating a root element where all whole react app will goes in
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* <Route
+          path={["/search", "/images", "/videos", "/news"]}
+          element={<Results />}
+        /> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// APP will get render inside root that we created
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class", //enabling the dark mode variant in tailwind css otherwise it will not work accordingly
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
